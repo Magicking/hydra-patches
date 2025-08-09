47d5f1ab423e15d841db3a6f299ec87f5bdd0588
Magicking <s@6120.eu>
2025-08-09 17:05:26 +0200

    chore: add fetedelamusic(may need cleaning) and gabber

---
+++ gabber.js
@@ -0,0 +1,11 @@
+bpm=220
+osc().scale(()=>a.fft[0]).color([1],[1,0],0)
+  .blend(osc(30,0.1).color(.8,.8,0))
+  .add(shape([3,42,3].smooth()).scale(()=>a.fft[1]*3))
+  .diff(shape([3,42,3].smooth()).scale(()=>a.fft[1]*1.3))
+  .diff(shape([3,42,3].smooth()).scale(()=>a.fft[1]*1.1))
+  .repeat([3,4],[3,4])
+  .blend(o0,0.6)
+  .out()
+
+a.show()
