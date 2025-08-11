e1e80510ba8ea42906450adc6211152049af0ea5
Lint Action <lint-action@samuelmeuli.com>
2025-08-11 21:37:44 +0000

    Fix code style issues with Prettier

--- lambda-1.js
+++ lambda-1.js
@@ -4,7 +4,7 @@
 
 await loadScript('https://6120.eu/public/midi.js')
 await loadScript('https://6120.eu/public/hh/hydra-text.js')
-setResolution(2500,2500)
+setResolution(2500, 2500)
 // Use midi messages from all channels of all inputs.
 await midi.start().show()
 a.show()
@@ -21,5 +21,5 @@ shape([3, 4, 11, 22].fast(), () => a.fft[0] / 3)
   )
   .add(src(o0), 0.98)
   //blend(src(o1), 0.9).
-  .repeat(1.1,1.1)
+  .repeat(1.1, 1.1)
   .out(o0)
