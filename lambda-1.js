ea358326b5214bbc30645ccdddf10f436d2ea16a
Magicking <Magicking@users.noreply.github.com>
2025-08-11 23:37:30 +0200

    hyper-hydra.glitch.mehyper-hydra.glitch.mehyptch.er-hhyper-hydra.glitch.meydra.ghyper-hydra.glitch.meydra.glime

---
+++ lambda-1.js
@@ -0,0 +1,25 @@
+// License Beerware
+// Sylvain "Magicking" Laurent
+// Midi controlling with note cc
+
+await loadScript('https://6120.eu/public/midi.js')
+await loadScript('https://6120.eu/public/hh/hydra-text.js')
+setResolution(2500,2500)
+// Use midi messages from all channels of all inputs.
+await midi.start().show()
+a.show()
+
+shape([3, 4, 11, 22].fast(), () => a.fft[0] / 3)
+  .color(
+    () => a.fft[0] / 2,
+    () => a.fft[1] / 2,
+    () => a.fft[3] / 2
+  )
+  .scroll(
+    () => a.fft[0],
+    () => a.fft[3]
+  )
+  .add(src(o0), 0.98)
+  //blend(src(o1), 0.9).
+  .repeat(1.1,1.1)
+  .out(o0)
