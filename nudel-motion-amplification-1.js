9df2d04204bd53b819192e78e1963eb5eeb60487
Magicking <Magicking@users.noreply.github.com>
2025-10-26 23:58:34 +0100

    Add motion amplification effect in nudel-motion-amplification-1.js
    Implement motion amplification using Voronoi and FFT.

---
+++ nudel-motion-amplification-1.js
@@ -0,0 +1,8 @@
+voronoi(100,3,()=>Math.sin(time)*Math.PI).mask(shape([2,3,4,6,7].fast())).repeat(4,4).rotate(()=>fft(0,1)).invert()
+.mult(voronoi(()=>fft(0)*30).color([1,0].smooth(),[0,.5].smooth(),1))
+.out(o0)
+/* Motion amplification by lagging in a buffer*/
+src(o0).out(o1)
+src(o1).diff(o0).blend(o1,0.1/*How shadowy, higher is less */).out(o3)
+/* En of motion amplification */
+render(o3) // <-output
