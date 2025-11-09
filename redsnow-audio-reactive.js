df25bb68b48337156d01316bef9def6a1c247394
Magicking <Magicking@users.noreply.github.com>
2025-11-09 01:40:46 +0100

    Add audio reactive visual effects in redsnow-audio-reactive.js

---
+++ redsnow-audio-reactive.js
@@ -0,0 +1,12 @@
+shape(99,.4).color(1,0,0)
+  .add(shape(3).rotate(()=>Math.sin(time/500)*360+45),1)
+  .add(shape(3).rotate(()=>-Math.sin(time/500)*360+45),1)
+  .scroll([.1,.9].ease('easeInCubic'),[.1,.9].smooth())
+  .scale(()=>a.fft[0]*2+0.1)
+  .out(o1)
+src(o1).repeat([20],[20])
+  .mask(shape(99).scroll(()=>0.1*Math.random()+a.fft[0]/3,()=>0.01*Math.random()+a.fft[1]/3).
+       add(shape(2,.1).scroll(0,[.35,.6].ease('easeInQuad'))))
+.add(src(o1).scale(.8).scroll(.3,.3))
+  .out()
+a.show()
