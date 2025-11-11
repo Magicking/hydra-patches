9e5054b1f5ef9ee4ffc5bd9955ee2e6db7ab6756
Lint Action <lint-action@samuelmeuli.com>
2025-11-11 11:56:58 +0000

    Fix code style issues with Prettier

--- highbar-audio-react-v2.js
+++ highbar-audio-react-v2.js
@@ -1,18 +1,28 @@
 setResolution(2000, 2000)
-shape(2, () => a.fft[0] + .3)
-	.repeat(3, 6)
-	.modulate(noise(() => a.fft[0]))
-	.color(1, 0, 2)
-	.mask(voronoi(3)
-		.scale(() => a.fft[0]))
-	.out(o1)
+shape(2, () => a.fft[0] + 0.3)
+  .repeat(3, 6)
+  .modulate(noise(() => a.fft[0]))
+  .color(1, 0, 2)
+  .mask(voronoi(3).scale(() => a.fft[0]))
+  .out(o1)
+src(o1).out(o2)
 src(o1)
-	.out(o2)
-src(o1)
-	.diff(o2)
-	.mult(solid([0, 1, 2].smooth(), [0, .3, .4, .6, .8, .9, 1].smooth(.2), [1, 2, 0].smooth()))
-	.mult(solid(()=>1-a.fft[1],()=>1-a.fft[2],()=>1-a.fft[3]))
-	.blend(o0, .6)
-	.saturate(1.5)
-	.out(o0)
-a.setSmooth(.6)
+  .diff(o2)
+  .mult(
+    solid(
+      [0, 1, 2].smooth(),
+      [0, 0.3, 0.4, 0.6, 0.8, 0.9, 1].smooth(0.2),
+      [1, 2, 0].smooth()
+    )
+  )
+  .mult(
+    solid(
+      () => 1 - a.fft[1],
+      () => 1 - a.fft[2],
+      () => 1 - a.fft[3]
+    )
+  )
+  .blend(o0, 0.6)
+  .saturate(1.5)
+  .out(o0)
+a.setSmooth(0.6)
