dcbb2e5a417164825eb08da55d38bb09230074bf
Lint Action <lint-action@samuelmeuli.com>
2026-03-29 20:15:28 +0000

    Fix code style issues with Prettier

--- jam-norns.js
+++ jam-norns.js
@@ -3,7 +3,7 @@ await loadScript('https://h.6120.eu/hydra-text.js')
 await loadScript('https://h.6120.eu/hydra-fractals.js')
 await loadScript('https://h.6120.eu/hydra-outputs.js')
 await loadScript('https://h.6120.eu/hydra-colorspaces.js')
-setResolution(2000,2000)
+setResolution(2000, 2000)
 oS.setLinear()
 await midi.start().show()
 
@@ -100,8 +100,8 @@ shape(vn, 1 / sn)
       .rotate(() => time / 9 + a.fft[0])
   )
   .scale(0.3)
-  .blend(o1,[.6,.1])
-  .blend(o2,.8)
+  .blend(o1, [0.6, 0.1])
+  .blend(o2, 0.8)
   .out(o1)
 a.setScale(1.5)
 a.setBins(8)
@@ -115,25 +115,38 @@ crown = new Array(13).fill().reduce(
     ),
   solid()
 )
-nnote=55
+nnote = 55
 m = midi.input().channel('*')
-m.onNote("*", ({ note, velocity, channel }) => { 
-  console.log("onCCevent")
-  console.log(note) 
+m.onNote('*', ({ note, velocity, channel }) => {
+  console.log('onCCevent')
+  console.log(note)
   console.log(velocity)
   console.log(channel)
-  nnote=note
-});
+  nnote = note
+})
 crown
   .layer(voronoi().mask(noise(20).modulate(noise(10).luma(0.4, 0.9), 0.2)))
- .scale(()=>(nnote-48.0)/12)
-//   .scale(()=>a.fft[0])
+  .scale(() => (nnote - 48.0) / 12)
+  //   .scale(()=>a.fft[0])
   .out(o2)
 a.show()
 // licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
 // by Nelson Vera
 // twitter: @nel_sonologia
 
-osc(8,-0.5, 1).color(-1.5, -1.5, -1.5).blend(o0).rotate(-0.5, -0.5).modulate(shape(4).rotate(0.5, 0.5).scale(2).repeatX(2, 2).modulate(o0, () => mouse.x * 0.0005).repeatY(2, 2)).blend(o1,[.7,.7].smooth()).blend(o0,.9).out(o0)
+osc(8, -0.5, 1)
+  .color(-1.5, -1.5, -1.5)
+  .blend(o0)
+  .rotate(-0.5, -0.5)
+  .modulate(
+    shape(4)
+      .rotate(0.5, 0.5)
+      .scale(2)
+      .repeatX(2, 2)
+      .modulate(o0, () => mouse.x * 0.0005)
+      .repeatY(2, 2)
+  )
+  .blend(o1, [0.7, 0.7].smooth())
+  .blend(o0, 0.9)
+  .out(o0)
 render(o0)
-
